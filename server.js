c6a2a35279a584b5c3991ca216b2333ae68f7a37
Chandrababugowda <chandrababugowda3042003@gmail.com>
2024-04-03 18:15:53 +0530

    Add the start script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const app = require("./mentoringPortal");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 
 // Connect to database
